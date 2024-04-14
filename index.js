1ff2d72f9d82468cf0b9b2b320536a5d5a93d337
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-14 21:24:25 +0530

    mongoose connect

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
+
+mongoose.connect("mongodb://127.0.0.1:27017/testt")
+    .then(() => {
+        console.log("Mongo Connection Open!!!");
+    })
+    .catch(err => {
+        console.log("Oh No, Mongo Connection Error!!!");
+        console.log(err);
+    })
+
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
