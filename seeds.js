244db71d6d4c18eab2b112a73a7e97f977e92f49
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-14 21:43:04 +0530

    add many items

--- seeds.js
+++ seeds.js
@@ -12,15 +12,54 @@ mongoose.connect("mongodb://127.0.0.1:27017/farmStand")
         console.log(err);
     })
 
-const p = new Product({
-    name: "Ruby Greek Fruit",
-    price: 1.99,
-    category: "Fruit"
-})
-p.save()
-    .then(p => {
-        console.log(p);
+// const p = new Product({
+//     name: "Ruby Greek Fruit",
+//     price: 1.99,
+//     category: "Fruit"
+// })
+// p.save()
+//     .then(p => {
+//         console.log(p);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
+
+
+// above way commented because we wanted to make many items and use above method needs to make one and save it, that's why using insertMany
+
+const seedProducts = [
+    {
+        name: "Fairy Eggplant",
+        price: 1.00,
+        category: "vegetable"
+    },
+    {
+        name: "Organic Goddness Melon",
+        price: 4.99,
+        category: "fruit"
+    },
+    {
+        name: "Organic Mini Seedless Watermelon",
+        price: 3.99,
+        category: "fruit"
+    },
+    {
+        name: "Organic Celery",
+        price: 1.50,
+        category: "vegetable"
+    },
+    {
+        name: "Chocolate Whole Milk",
+        price: 2.69,
+        category: "dairy"
+    }
+]
+
+Product.insertMany(seedProducts)
+    .the(res => {
+        console.log(res);
     })
-    .catch(err => {
-        console.log(err);
+    .catch(e => {
+        console.log(e)
     })
