d18fa888c328021e6184831c7f7b5170ec381cc6
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-16 12:42:44 +0530

    complete D from CRUD

--- index.js
+++ index.js
@@ -59,6 +59,12 @@ app.put("/products/:id", async (req, res) => {
     res.redirect(`/products/${product._id}`);
 })
 
+app.delete("/products/:id", async (req, res) => {
+    const { id } = req. params;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    res.redirect("/products");
+})
+
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
 })
